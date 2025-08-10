de80920c54f5116abedb7aed2b9464489720a12f
Vishal <vishal.s@neosoftmail..com>
2025-08-10 21:41:17 +0530

    New Handler added in the project.

--- src/main/java/com/example/kafka_demo/DemoController/Controller/UserController.java
+++ src/main/java/com/example/kafka_demo/DemoController/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.kafka_demo.DemoController.Controller;
 
 import com.example.kafka_demo.DemoController.Service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class UserController {
     public String userRegistration(){
 //        registrationService.registerAllUser();
         return "User successfully Registered";
+    }
+
+    @GetMapping("/")
+    public String helloService(){
+        return "Hii Vishal";
     }
 
 
